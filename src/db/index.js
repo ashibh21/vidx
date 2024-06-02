2f29c77b0e7c0ffe72c3a35e78523b5527dc5df6
Ashish bhatt <98805495+ashibh21@users.noreply.github.com>
2024-06-02 09:14:00 +0530

    initilized db folder and changed db connection

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+const connectDB = async () => {
+  try {
+    const connInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection Failed", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
