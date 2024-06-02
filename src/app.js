d4f7fd5e5ac76623b4ea75009f6dceb0161149bd
Ashish bhatt <98805495+ashibh21@users.noreply.github.com>
2024-06-03 00:57:43 +0530

    initilized middleware and added cookieParser and cros

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookieParser";
 
 const app = express();
 
-export { app };
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(epress.json({ limit: "16kb" }));
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"));
+
+app.use(cookieParser())
+
+
+export default app;
